7b35d6e2c594b576e5cdbe67b0018044f2ec7f1f
Junjun Mao <junjun.mao@gmaile.com>
2019-08-01 14:45:56 -0400

    update

---
+++ bin/verify_tpl.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+This program verifies one ftpl file over the ligand pdb file, and writes out the mcce pdb file.
+"""
+
+import sys
+from param import *
+
+if __name__ == "__main__":
