ffae2c85c8ee46c06e30c853323d0147a740bdc3
Junjun Mao <junjun.mao@gmail.com>
2019-08-01 22:31:27 -0400

    update

--- bin/param.py
+++ bin/param.py
@@ -13,7 +13,32 @@ class Env:
         return
 
     def load_ftpl(self, fname):
-        """Load a tpl file"""
+        """Load a ftpl file"""
 
+        # default string values, or defined as below
+        float_values = ["EXTRA", "SCALING"]
+        int_values = []
 
+        logging.info("   Loading ftpl file %s" % fname)
+        lines = open(fname).readlines()
+        for line in lines:
+            line = line.split("#")[0]
+            fields = line.split(":")
+            if len(fields) != 2:
+                continue
 
+            key_string = fields[0].strip()
+            keys = key_string.split(",")
+            keys = [x.strip().strip("\"") for x in keys]
+            keys = [x for x in keys if x]
+            keys = tuple(keys)
+
+            value_string = fields[1].strip()
+            if keys[0] in float_values:
+                self.tpl[keys] = float(value_string)
+            elif keys[0] in int_values:
+                self.tpl[keys] = int(value_string)
+            else:
+                self.tpl[keys] = value_string
+
+        return
