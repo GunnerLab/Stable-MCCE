f50ab5c180576d5a521e2dcefb05caa95cef46fe
Junjun Mao <junjun.mao@gmail.com>
2019-10-28 11:41:13 -0400

    update ftpl from Develop version

---
+++ bin/tpl-free2mcce.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+This program converts ftpl files in param ftpl directory and extra.tpl into a single mcce tpl file.
+Any missing parameters will be loaded from a default tpl file "default.ftpl"
+"""
+
+import sys
+from pymccelib import *
+import logging
+
+
+def convert_free2mcce():
+    
+
+    return
+
+if __name__ == "__main__":
+    convert_free2mcce()
