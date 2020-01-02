c51f4c3ae17f6eb2ba20b27fdf19eea40330ae7c
Junjun Mao <junjun.mao@gmail.com>
2020-01-02 16:02:23 -0500

    update

--- bin/translate_step2.py
+++ bin/translate_step2.py
@@ -2,8 +2,15 @@
 
 import sys
 
+CONVERT = {"1H   HOH" : " H1  HOH"
+
+}
+
 if __name__ == "__main__":
 
     lines = open(sys.argv[1]).readlines()
     for line in lines:
-        print(line, end="")
+        if line[:6] == "ATOM  " or line[:6] == "HETATM":
+            if line[12:20] in CONVERT:
+                print(line[:12]+CONVERT[line[12:20]]+line[20:].rstrip())
+
