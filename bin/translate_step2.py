3babefa298c93301ceeab0e85ff0e0221d7d8a17
Junjun Mao <junjun.mao@gmail.com>
2020-01-05 17:06:16 -0500

    update

--- bin/translate_step2.py
+++ bin/translate_step2.py
@@ -2,9 +2,21 @@
 
 import sys
 
-CONVERT = {"1H   HOH" : " H1  HOH"
+CONVERT = {"1H   HOH": " H1  HOH",
+           "2H   HOH": " H2  HOH",
+           "1HG1 VAL": "HG11 VAL",
+           "2HG1 VAL": "HG12 VAL",
+           "3HG1 VAL": "HG13 VAL",
+           "1HG2 VAL": "HG21 VAL",
+           "2HG2 VAL": "HG22 VAL",
+           "3HG2 VAL": "HG23 VAL",
+           "1HA  GLY": " HA2 GLY",
+           "2HA  GLY": " HA3 GLY",
+           "1HB  ALA": " HB1 ALA",
+           "2HB  ALA": " HB2 ALA",
+           "3HB  ALA": " HB3 ALA",
 
-}
+           }
 
 if __name__ == "__main__":
 
@@ -15,5 +27,6 @@ if __name__ == "__main__":
                 print(line[:12]+CONVERT[line[12:20]]+line[20:].rstrip())
             else:
                 print(line, end="")
-        print(line,end="")
+        else:
+            print(line, end="")
 
