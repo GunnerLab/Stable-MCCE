ed3aeb091efdf32f0c1c253fed5ec8815d0caf82
Junjun Mao <junjun.mao@gmaile.com>
2020-05-26 17:46:34 -0400

    update

--- bin/step5.py
+++ bin/step5.py
@@ -24,9 +24,38 @@ Usage examples:
 """
 
 import argparse
+import sys
 import numpy as np
 
 PH2KCAL = 1.364
+
+class Conformer:
+    def __init__(self):
+        self.name = ""
+        self.occ = []
+        self.crg = 0.0
+        self.em0 = 0.0
+        self.pka0 = 0.0
+        self.ne = 0
+        self.nh = 0
+        self.vdw0 = 0.0
+        self.vdw1 = 0.0
+        self.tors = 0.0
+        self.epol = 0.0
+        self.dsolv = 0.0
+        self.extra = 0.0
+        return
+
+class Residue:
+    def __init__(self):
+        self.resid = ""
+        self.charge_type = "0"
+        self.conformers = []
+        self.netcrg = []
+        self.ground_state = []
+        self.charged_state = []
+
+        return
 
 class Titration:
     def __init__(self, args):
@@ -37,7 +66,23 @@ class Titration:
         conformers = self.load_confs()
         self.residues = self.group_residues(conformers)
 
+        # get sum_crg
+        npoints = len(self.titration_points)
+
+        for res in self.residues:
+            net_crg = [0.0 for i in range(npoints)]
+            for conf in res.conformers:
+                net_crg = [net_crg[i]+conf.crg*conf.occ[i] for i in range(npoints)]
+            res.netcrg = net_crg
+
+
+
+        return
+
+
     def load_confs(self):
+        conformers = []
+
         fort38_lines = open("fort.38").readlines()
         head3_lines = open("head3.lst").readlines()
         line = fort38_lines.pop(0)
@@ -46,7 +91,53 @@ class Titration:
         self.titration_points = [float(x) for x in fields[1:]]
         head3_lines.pop(0)
 
+        while fort38_lines and head3_lines:
+            fort38_data = fort38_lines.pop(0)
+            head3_data = head3_lines.pop(0)
+            conf = Conformer()
+
+            fields = fort38_data.split()
+            conf.name = fields[0]
+            conf.occ = [float(x) for x in fields[1:]]
+
+            fields = head3_data.split()
+            if conf.name != fields[1]:
+                print("Confomer %s in fort.38 does not match %s in head3.lst" % (conf.name, fields[1]))
+                sys.exit()
+
+            conf.crg = float(fields[4])
+            conf.em0 = float(fields[5])
+            conf.pka0 = float(fields[6])
+            conf.ne = int(fields[7])
+            conf.nh = int(fields[8])
+            conf.vdw0 = float(fields[9])
+            conf.vdw1 = float(fields[10])
+            conf.tors = float(fields[11])
+            conf.epol = float(fields[12])
+            conf.dsolv = float(fields[13])
+            conf.extra = float(fields[14])
+            conformers.append(conf)
+
+        return conformers
+
+
     def group_residues(self, conformers):
+        res_ids = []
+        residues = {}
+        for conf in conformers:
+            resid = conf.name[:3]+conf.name[5:11]
+            if resid in res_ids:
+                res = residues[resid]
+            else:
+                res = Residue()
+                res.resid = resid
+                residues[resid] = res
+                res_ids.append(resid)
+            res.conformers.append(conf)
+
+
+        return [residues[x] for x in res_ids]
+
 
 if __name__ == "__main__":
 
