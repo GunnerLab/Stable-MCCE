0ac2c955cccda77916946e5bcdefeb5a71af21e8
Junjun Mao <junjun.mao@gmail.com>
2020-10-23 19:40:50 -0400

    update

---
+++ bin/ms_analysis.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+
+import sys
+import math
+import numpy as np
+
+ph2Kcal = 1.364
+Kcal2kT = 1.688
+
+
+class Microstate:
+    def __init__(self, state, E, count):
+        self.state = state
+        self.E = E
+        self.count = count
+
+
+class Conformer:
+    def __init__(self):
+        self.iconf = 0
+        self.confid = ""
+        self.resid = ""
+        self.occ = 0.0
+        self.crg = 0.0
+
+    def load_from_head3lst(self, line):
+        fields = line.split()
+        self.iconf = int(fields[0]) - 1
+        self.confid = fields[1]
+        self.resid = self.confid[:3]+self.confid[5:11]
+        self.crg = float(fields[4])
+
+class MSout:
+    def __init__(self):
+        self.T = 273.15
+        self.pH = 7.0
+        self.Eh = 0.0
+        self.N_ms = 0
+        self.lowest_E = 0.0
+        self.highest_E = 0.0
+        self.average_E = 0.0
+        self.fixed_iconfs = []
+        self.fixed_crg = 0.0
+        self.fixed_ne = 0.0
+        self.fixed_nh = 0.0
+        self.free_residues = []   # free residues, referred by conformer indices
+        self.iconf2ires = {}      # from conformer index to free residue index
+        self.microstates = {}
+        self.conformers = []
+
+    def load_msout(self, fname):
+        lines = open(fname).readlines()
+
+        # Get a valid line
+        while True:
+            line = lines.pop(0).strip()
+            if len(line) > 0 and line[0] != "#":
+                break
+
+        fields = line.split(",")
+        for field in fields:
+            key, value = field.split(":")
+            key = key.strip().upper()
+            value = float(value)
+            if key == "T":
+                self.T = value
+            elif key == "PH":
+                self.pH = value
+            elif key == "EH":
+                self.Eh = value
+
+        # second line, confirm this is from Monte Carlo sampleing
+        while True:
+            line = lines.pop(0).strip()
+            if len(line) > 0 and line[0] != "#":
+                break
+
+        key, value = line.split(":")
+        if key.strip() != "METHOD" or value.strip() != "MONTERUNS":
+            print("This file %s is not a valid microstate file" % fname)
+            sys.exit(-1)
+
+        # Third line, fixed conformer indicies
+        while True:
+            line = lines.pop(0).strip()
+            if len(line) > 0 and line[0] != "#":
+                break
+
+        _, iconfs = line.split(":")
+        self.fixed_iconfs = [int(i) for i in iconfs.split()]
+
+        # 4th line, free residues
+        while True:
+            line = lines.pop(0).strip()
+            if len(line) > 0 and line[0] != "#":
+                break
+
+        _, residues_str = line.split(":")
+        residues = residues_str.split(";")
+        self.free_residues = []
+        for f in residues:
+            if f.strip():
+                self.free_residues.append([int(i) for i in f.split()])
+        for i_res in range(len(self.free_residues)):
+            for iconf in self.free_residues[i_res]:
+                self.iconf2ires[iconf] = i_res
+
+        # find the next MC record
+        found_mc = False
+        newmc = False
+        self.N_ms = 0
+
+        for line in lines:
+            if line.find("MC:") == 0:   # ms starts
+                found_mc = True
+                newmc = True
+                continue
+            elif newmc:
+                f1, f2 = line.split(":")
+                current_state = [int(c) for c in f2.split()]
+                newmc = False
+                continue
+            elif found_mc:
+                fields = line.split(",")
+                if len(fields) >= 3:
+                    state_e = float(fields[0])
+                    count = int(fields[1])
+                    flipped = [int(c) for c in fields[2].split()]
+
+                    for ic in flipped:
+                        ir = self.iconf2ires[ic]
+                        current_state[ir] = ic
+
+                    ms = Microstate(list(current_state), state_e, count)
+                    key = ",".join(["%d" % i for i in ms.state])
+                    if key in self.microstates:
+                        self.microstates[key].count += ms.count
+                    else:
+                        self.microstates[key] = ms
+
+        # find N_ms, lowerst, highst, averge E
+        self.N_ms = 0
+        E_sum = 0.0
+        self.lowest_E = next(iter(self.microstates.values())).E
+        self.highest_E = next(iter(self.microstates.values())).E
+        for ms in self.microstates.values():
+            self.N_ms += ms.count
+            E_sum += ms.E * ms.count
+            if self.lowest_E > ms.E:
+                self.lowest_E = ms.E
+            if self.highest_E < ms.E:
+                self.highest_E = ms.E
+        self.average_E = E_sum / self.N_ms
+
+
+if __name__ == "__main__":
+    msout = MSout()
+    msout.load_msout("ms_out/pH4eH0ms.txt")
+    print(msout.T)
+    print(msout.highest_E)
+    print(msout.lowest_E)
+    print(msout.average_E)
+    print(msout.N_ms)
