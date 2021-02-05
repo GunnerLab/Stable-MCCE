4f2beef16aedde1cfd8c498226b037e0cfadda58
Junjun Mao <junjun.mao@gmail.com>
2021-02-04 23:16:55 -0500

    improved diff_mc.py, closes #207

--- bin/diff_mc.py
+++ bin/diff_mc.py
@@ -3,65 +3,97 @@
 
 import os, argparse
 
-def read_matrix(fname):
-    "Read a file into a matrix, str as str, numbers to float"
-    result = []
-    lines = open(fname).readlines()
-    for line in lines:
+class MCfile:
+    def __init__(self):
+        self.type = ""
+        self.pHs = []
+        self.names = []
+        self.values = {}
+
+    def readfile(self, fname):
+        lines = open(fname).readlines()
+        line = lines.pop(0)
         fields = line.strip().split()
-        ldata = []
-        for f in fields:
-            try:
-                ldata.append(float(f))
-            except ValueError:
-                ldata.append(f)
-        result.append(ldata)
+        self.type = fields[0]
+        if self.type.upper() == "PH":
+            self.pHs = ["%6.1f" % float(x) for x in fields[1:]]
+        else:
+            self.pHs = ["%6.f" % float(x) for x in fields[1:]]
+        for line in lines:
+            fields = line.strip().split()
+            if len(fields) > 1:
+                name = fields[0]
+                self.names.append(name)
+                for i in range(len(fields)-1):
+                    self.values[(name, self.pHs[i])] = fields[i+1]
 
-    return result
+    def printme(self):
+        print("%-14s %s" % (self.type, " ".join(self.pHs)))
+        for name in self.names:
+            print("%-14s %s" % (name, " ".join(["%6s" % self.values[(name, ph)] for ph in self.pHs])))
+
+def merge_lists(list1, list2):
+    "Return a merged list while preserving order."
+
+    ipos = 0
+    list_merged = []
+    for x in list1:
+        if x not in list_merged:
+            if x in list2:
+                xpos = list2.index(x)
+                list_merged += list2[ipos:xpos]
+                ipos = xpos + 1
+            list_merged.append(x)
+
+    # list2 might have extra items
+    if len(list2) > ipos:
+        list_merged += list2[ipos:]
+
+    return list_merged
 
 
-def msubtract(m1, m2):
-    nrow_m1 = len(m1)
-    nrow_m2 = len(m2)
-    result = []
-    if nrow_m1 != nrow_m2:
-        print("Rows in two data files don't match")
+def diff(f1, f2):
+    if f1.type != f2.type:
         return None
 
-    result = [m1[0]]
-    for irow in range(1, nrow_m1):
-        if len(m1[irow]) != len(m2[irow]):
-            print("Columns %s in two data files don't match" % m1[irow][0])
-            return None
-        row_result = []
-        for icol in range(len(m1[irow])):
-            if isinstance(m1[irow][icol], str):  # keep
-                row_result.append(m1[irow][icol])
-            else:
-                row_result.append(m1[irow][icol] - m2[irow][icol])
-        result.append(row_result)
+    # get merged pH
+    pHs = merge_lists(f1.pHs, f2.pHs)
 
-    return result
+    # get merged names
+    names = merge_lists(f1.names, f2.names)
 
+    values = {}
+    for name in names:
+        for ph in pHs:
+            key = (name, ph)
+            if key in f1.values and key not in f2.values:
+                values[key] = "<<<"
+            elif key not in f1.values and key in f2.values:
+                values[key] = ">>>"
+            else:  # must be in both
+                values[key] = float(f1.values[key]) - float(f2.values[key])
 
-def diff(files):
-    data1 = read_matrix(files[0])
-    data2 = read_matrix(files[1])
-    data_diff = msubtract(data1, data2)
+    delta = MCfile()
+    delta.type = f1.type
+    delta.pHs = pHs
+    delta.names = names
+    delta.values = values
 
-    if data_diff:
-        for row_data in data_diff:
-            print("%14s %s" % (row_data[0], " ".join(["%6.3f" % x for x in row_data[1:]])))
-
-    return
-
+    return delta
 
 if __name__ == "__main__":
 
     # Get the command arguments
-    helpmsg = "Subtract two sum_crg.out or fort.38 files."
+    helpmsg = "Subtract two sum_crg.out or fort.38 files. The first column is used as matching IDs of lines."
     parser = argparse.ArgumentParser(description=helpmsg)
     parser.add_argument("files", metavar="file1 file2", nargs=2)
     args = parser.parse_args()
 
-    diff(args.files)
+    f1 = MCfile()
+    f1.readfile(args.files[0])
+
+    f2 = MCfile()
+    f2.readfile(args.files[1])
+
+    delta = diff(f1, f2)
+    delta.printme()
