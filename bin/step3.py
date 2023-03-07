c4b717f63bb0961883a1a31af68b55f1c723fc8e
newbooks <junjun.mao@gmail.com>
2023-03-06 21:28:49 -0500

    hot fix step3.py --norun

--- bin/step3.py
+++ bin/step3.py
@@ -145,20 +145,21 @@ def process(args):
 
             logging.info("Main: Done")
 
-    # Recalculate vdw
-    if os.path.exists("energies_bak") and os.path.isdir("energies_bak"):
-        shutil.rmtree("energies_bak")
-    shutil.copytree("energies", "energies_bak")
-    shutil.copy("head3.lst", "head3.lst_bak")
+        # Recalculate vdw
+        if os.path.exists("energies_bak") and os.path.isdir("energies_bak"):
+            shutil.rmtree("energies_bak")
+        shutil.copytree("energies", "energies_bak")
+        shutil.copy("head3.lst", "head3.lst_bak")
 
-    pdbfile = "step2_out.pdb"
-    protein = Protein()
-    protein.loadpdb(pdbfile)
-    protein.make_connect12()
-    protein.make_connect13()
-    protein.make_connect14()
-    protein.calc_vdw()
-    update_opp(protein)
+        pdbfile = "step2_out.pdb"
+
+        protein = Protein()
+        protein.loadpdb(pdbfile)
+        protein.make_connect12()
+        protein.make_connect13()
+        protein.make_connect14()
+        protein.calc_vdw()
+        update_opp(protein)
 
     return
 
