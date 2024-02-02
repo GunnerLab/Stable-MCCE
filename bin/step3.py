5fa02e7fceb7025dca1bb0178c7dd9afe1af0b62
newbooks <junjun.mao@gmail.com>
2024-02-02 15:06:34 -0500

    update

--- bin/step3.py
+++ bin/step3.py
@@ -319,7 +319,6 @@ def pbe(iric):
         else:
             print("No compatible PBE solver detected, given pb solver is %s" % run_options.s)
 
-
         # switch back to the current directory
         if not run_options.debug:
             shutil.rmtree(tmp_pbe)
